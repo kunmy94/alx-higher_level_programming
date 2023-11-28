2c1dd9391928bbc0135e4b7f387cae66d47a614d
Adelakun <kunmyboi94@gmail.com>
2023-11-28 01:58:15 -0800

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = " programming"
-str3 = " with Python"
-str4 = str1 + str2 + str3
-print(str4)
+print("object" + "-" 
+        + "oriented" 
+        + " programming" 
+        + " with Python")
